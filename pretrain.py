37e105dffe7abe06c756919593113472ff7fac79
jjwwhh0416 <jjwwhh0416@gmail.com>
2026-07-09 14:22:02 +0900

    wandb

--- pretrain.py
+++ pretrain.py
@@ -15,7 +15,7 @@ def main_worker():
     world_size = int(os.environ["WORLD_SIZE"])
 
     torch.cuda.set_device(local_rank)
-    dist.init_process_group(backend="gloo")
+    dist.init_process_group(backend="nccl")
 
     # --------- Load Config ----------
     parser = argparse.ArgumentParser()
